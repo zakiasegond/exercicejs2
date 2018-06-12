b7719a8d2d9cec4b0a893f161a653aed8181a353
zakiasegond <segondzakia@live.fr>
2018-06-12 16:49:10 +0200

    exo 4 en cours

---
+++ JS-exercice-master/functions/fonction.js
@@ -0,0 +1,23 @@
+// exo 1 Faire une fonction qui retourne 'true'. 
+//pour retourner une fonction, utiliser 'return'
+function fonction1()
+{
+	return true;
+
+}
+
+//exo 2 faire une fonction qui prend en paramètre une chaine de caracteres et qui renvois cette meme chaine.
+function fonction2(string){
+var string= "bonjour je m'appelle zakia";
+alert(string);
+	return string;
+}
+
+
+//exo 3 faire une fonction qui prend 2 chaine de caracteres et qui renvois son result!
+function fonction3(){
+var texte1= "salut ";
+var texte2= "les copains";
+alert(texte1 + texte2);
+
+}
