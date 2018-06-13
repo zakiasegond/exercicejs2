876bd01f2890195bd9b4d685bb20fa3549bf358b
zakiasegond <segondzakia@live.fr>
2018-06-13 16:53:21 +0200

    exo loop terminés

--- JS-exercice-master/loop/boucle.js
+++ JS-exercice-master/loop/boucle.js
@@ -38,7 +38,7 @@ for (x = 100; x >= 10; x--)
 	var result = x*y;
 	document.write(result + "||");
 }
-*/
+
 
 
 
@@ -58,22 +58,45 @@ while (var2 < 10)
 
 
 
-
-
-
-
-
-
-
-
 //Exercice 5 En allant de 1 à 15 avec un pas de 1, afficher le message On y arrive presque.
+var var1= 1;
+for (var1 = 1; var1 <= 15; var1++)
+{
+    document.write(var1);
+alert("On y arrive presque!");
+}
+
+
 
 //Exercice 6 En allant de 20 à 0 avec un pas de 1, afficher le message C'est presque bon.
+var var1= 20;
+for (var1 = 20; var1 > 0; var1--)
+{
+    document.write(var1);
+alert("C'est presque bon!");
+}
+
+
+
 
 //Exercice 7 En allant de 1 à 100 avec un pas de 15, afficher le message On tient le bon bout.
+var var1= 1;
+for (var1 = 1; var1 <= 100; var1+=15)
+{
+    document.write(var1);
+alert("On tient le bon bout!");
+}
+*/
+
+
 
 //Exercice 8 En allant de 200 à 0 avec un pas de 12, afficher le message Enfin !!!!.
-
+var var1= 200;
+for (var1 = 200; var1 >= 0; var1-=12)
+{
+    document.write(var1);
+alert("Enfin");
+}
 
 
 
